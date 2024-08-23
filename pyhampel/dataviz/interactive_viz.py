1a9e82aae751e7f61f34d58fe88b00428ab0ecb2
maxwarch <maximekuil@gmail.com>
2024-08-23 17:26:58 +0200

    change template layout

--- pyhampel/dataviz/interactive_viz.py
+++ pyhampel/dataviz/interactive_viz.py
@@ -165,7 +165,7 @@ def hampel_interactive_with_dev(df_list: list, key_col, orig_col: str, filtered_
 
     # Define FigureWidget that will contain traces
     g = go.FigureWidget(data=[trace1, trace2, trace3, trace4, trace5],
-                        layout=go.Layout(title=dict(text="Usage for " + str(df_list[0][key_col].unique()[0])), ))
+                        layout=go.Layout(template='plotly_white', title=dict(text="Usage for " + str(df_list[0][key_col].unique()[0])), ))
 
     # Define Handler Function
     # noinspection PyTypeChecker,PyUnusedLocal
